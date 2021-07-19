b4a54dc27d02d46505a875cfda31acbf8ddd4757
张宏海 <au000908@7kid.com>
2021-07-19 17:36:55 +0800

    制作坦克预制体,并实现坦克的移动旋转,摄像机的跟随,以及坦克炮塔跟随摄像机视野旋转

---
+++ Assets/Scripts/Game/Tank.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CameraFollow cameraFollow = null;
+
+    private Transform turret = null;
+    void Start()
+    {
+        turret = transform.Find("TankRenderers/TankTurret");
+        cameraFollow = Camera.main.transform.GetComponent<CameraFollow>();
+    }
+
+    private float moveSpeed = 10f;
+    private float unitRotationAngle = 30f;
+    private float rotationAngle = 0f;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //简单地控制坦克移动:上下左右键控制坦克移动的方向,每次按下按钮时相对应方向旋转并移动一个单位
+        // if(Input.GetKeyDown(KeyCode.UpArrow))
+        // {
+        //     transform.eulerAngles = new Vector3(0,0,0);
+        //     transform.position += transform.forward * moveSpeed;
+        // }
+        // else if(Input.GetKeyDown(KeyCode.DownArrow))
+        // {
+        //     transform.eulerAngles = new Vector3(0,180,0);
+        //     transform.position += transform.forward * moveSpeed;
+        // }
+        // else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        // {
+        //     transform.eulerAngles = new Vector3(0,-90,0);
+        //     transform.position += transform.forward * moveSpeed;
+        // }
+        // else if(Input.GetKeyDown(KeyCode.RightArrow))
+        // {
+        //     transform.eulerAngles = new Vector3(0,90,0);
+        //     transform.position += transform.forward * moveSpeed;
+        // }
+
+        //另一种控制方案:水平方向的按钮(左右方向上)控制坦克旋转;竖直方向的按钮(上下方向上)控制坦克前后移动
+        rotationAngle = Input.GetAxis("Horizontal") * unitRotationAngle * Time.deltaTime;
+        transform.Rotate(0,rotationAngle,0,Space.World);
+        transform.position += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime * transform.forward;
+        if(turret != null)
+            rotateTurret();
+    }
+    private void rotateTurret()
+    {
+        // turret.rotation = new Quaternion(0,Camera.main.transform.rotation.y,0,turret.rotation.w);
+        //由于使用Quaternion.Euler设置当前物体的旋转时,参数为正数时表示逆着旋转轴的正方向看过去顺时针方向为正方向,因此要将从cameraFollow中获取到的旋转角度取相反数才能正确旋转炮塔
+        //另外,修改rotation时最好使用Quaternion.Euler来进行设置比较好,不要直接使用new Vector4(x,y,z,w)这样的方式去设置.
+        turret.rotation = Quaternion.Euler(0f,-cameraFollow.FollowAngleInHorizontal,0);
+    }
+}
---
+++ Assets/Scripts/Utils/CameraFollow.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+    public Transform FollowTarget = null;
+    [Range(5f,15f)]
+    private float followInterVal = 15f;
+
+    private float followAngleInHorizontal = 0f;
+    public float FollowAngleInHorizontal {
+        get {
+            return followAngleInHorizontal;
+        }
+    }
+    private float followAngleInVertical = 30f;
+
+    // private float distanceInX = 0f;
+    private float distanceInY = 0f;
+    private float distanceInZ = 0f;
+
+    private float cameraRotateSpeed = 20f;
+    private float maxAngleInVertical = 60f;
+    private float minAngleInVertical = 0;
+
+    private float cameraZoomSpeed = 5f;
+    private float maxFollowInterval = 25f;
+    private float minFollowInterval = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(FollowTarget != null)
+        {
+            if(FollowTarget.Find("Camerapoint") != null)
+                this.FollowTarget = FollowTarget.Find("Camerapoint");
+            distanceInY = followInterVal * Mathf.Sin((Mathf.PI/180)*followAngleInVertical);
+            distanceInZ = followInterVal * Mathf.Cos((Mathf.PI/180)*followAngleInVertical);
+            followToTarget();
+        }
+    }
+
+    private void cameraRotate()
+    {
+        followAngleInHorizontal -= Input.GetAxis("Mouse X") * cameraRotateSpeed;//"Mouse X"代表鼠标横向移动增量,即鼠标向左移动时Input.GetAxis("Mouse X")返回的是负数的增量;鼠标向右移动时Input.GetAxis("Mouse X")返回的是正数的增量.因为我们希望鼠标向左移动时摄像机向左旋转,向右移动时摄像机向右旋转,又因为Unity中使用的是左手坐标系,即旋转时顺着旋转轴的正方向看过去时是顺时针为旋转时的正方向.这里我们是逆着Y轴的正方向去看,因此正方向反过来了,即逆时针为正方向,也就是说当前旋转角度的正值为向左旋转,负值为向右旋转,因此这里使用的是-=.
+
+        followAngleInVertical -= Input.GetAxis("Mouse Y") * cameraRotateSpeed;
+        
+
+        if(followAngleInVertical >= maxAngleInVertical)
+            followAngleInVertical = maxAngleInVertical;
+        else if(followAngleInVertical <= minAngleInVertical)
+            followAngleInVertical = minAngleInVertical;
+
+        distanceInY = followInterVal * Mathf.Sin((Mathf.PI/180)*followAngleInVertical);
+        distanceInZ = followInterVal * Mathf.Cos((Mathf.PI/180)*followAngleInVertical);
+    }
+
+    private void cameraZoom()
+    {
+        this.followInterVal += Input.GetAxis("Mouse ScrollWheel") * cameraZoomSpeed;
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)//往上滚动
+        {
+            if(this.followInterVal > maxFollowInterval)
+                this.followInterVal = maxFollowInterval;
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)//往下滚动
+        {
+            if(this.followInterVal < minFollowInterval)
+                this.followInterVal = minFollowInterval;
+        }
+    }
+
+    private void followToTarget()
+    {
+        transform.position = new Vector3(FollowTarget.position.x+distanceInZ*Mathf.Sin((Mathf.PI/180)*followAngleInHorizontal),FollowTarget.position.y+distanceInY,FollowTarget.position.z-distanceInZ*Mathf.Cos((Mathf.PI/180)*followAngleInHorizontal));
+
+        transform.LookAt(FollowTarget);
+    }
+
+    void LateUpdate()
+    {
+        if(FollowTarget != null)
+        {
+            cameraRotate();
+            followToTarget();
+            cameraZoom();
+        }
+    }
+}
